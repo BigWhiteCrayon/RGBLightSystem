a5380a4780db1937ad1a48b69598efb9e4132b20
BigWhiteCrayon <jordanryoungberg@gmail.com>
2020-05-11 16:17:53 -0700

    wrote some test code and embedded webpage

--- index.js
+++ index.js
@@ -1,7 +1,57 @@
-const express = require('express')
-const app = express()
-const port = 80
+const express = require('express');
+const bodyParser = require('body-parser');
+const ip = require('ip');
+const hbs = require('express-handlebars');
+const Gpio = require('onoff').Gpio;
 
-app.get('/', (req, res) => res.send('this is a different test'))
+let red = new Gpio(27, {mode: Gpio.OUTPUT});
+let green = new Gpio(17, {mode: Gpio.OUTPUT});
+let blue = new Gpio(22, {mode: Gpio.OUTPUT});
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+const app = express();
+const port = 80;
+
+app.set('view engine', 'hbs');
+
+app.engine( 'hbs', hbs({
+    extname: 'hbs',
+}));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => res.render('index', {postURL: '/setColor', button:[
+    {
+        colorName: 'Red',
+        colorData: {
+            red: 255,
+            green: 0,
+            blue: 0
+        }
+    },
+    {
+        colorName: 'Green',
+        colorData: {
+            red: 0,
+            green: 255,
+            blue: 0
+        }
+    },
+    {
+        colorName: 'Blue',
+        colorData: {
+            red: 0,
+            green: 0,
+            blue: 255
+        }
+    }
+]}));
+
+app.post('/setColor', (req, res) => {
+    res.status(400);
+    red.pwmWrite(req.body.red);
+    green.pwmWrite(req.body.green);
+    blue.pwmWrite(req.body.blue);
+})
+
+app.listen(port, () => console.log(`listening at ${ip.address() + ':' + port}`));
